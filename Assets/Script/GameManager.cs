3fce0f52764058001f7ab41ed60df260e17160bf
Alwin_Joshy <flamencoresonance@gmail.com>
2019-07-22 16:40:22 +0400

    we started by adding unicorn...

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerObj;
+
+    void Start()
+    {
+        SpawnPlayer();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnPlayer()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/3, Screen.height/2, 1));
+        Instantiate(playerObj, pos, Quaternion.identity);
+    }
+
+}
