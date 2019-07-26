8ade3a2596fd528f4ff649a26dde37c9817c2f27
lilrusty <r.dsouza@saedubai.com>
2019-07-26 18:25:56 +0400

    Basic shooting without colliders and rate

---
+++ Assets/Script/LaserScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Move");
+        rb2d.AddForce(transform.right * force);
+        BoundaryCheck();
+    }
+    void BoundaryCheck()
+    {
+        if (transform.position.x >= 11)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
