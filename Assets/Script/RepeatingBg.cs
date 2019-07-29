989ced3f6237c9fc6c6fe1140e1f1725cfe1f0d1
lilrusty <r.dsouza@saedubai.com>
2019-07-30 00:25:31 +0400

    Parallax scrolling and repetetive backgrounds done

---
+++ Assets/Script/RepeatingBg.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBg : MonoBehaviour
+{
+    float horizontalLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        horizontalLength = GetComponent<SpriteRenderer>().bounds.size.x;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -horizontalLength)
+        {
+            Reposition();
+        }
+    }
+
+    void Reposition()
+    {
+        Vector2 offset = new Vector2(horizontalLength * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
+    }
+}
---
+++ Assets/Script/Scrolling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    GameManager manager;
+    public GameObject managerObject;
+
+    Rigidbody2D rb2d;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = managerObject.GetComponent<GameManager>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.velocity = new Vector2(manager.scrollSpeed * -speed, 0);
+    }
+}
