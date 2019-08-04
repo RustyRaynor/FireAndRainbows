34707180766ee3c6fb0d5a56c687d9b0afc9400f
lilrusty <r.dsouza@saedubai.com>
2019-08-04 19:08:33 +0400

    Made explosion when enemy gets hit

--- Assets/Script/LaserScript.cs
+++ Assets/Script/LaserScript.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class LaserScript : MonoBehaviour
 {
+    public GameObject explosion;
     Rigidbody2D rb2d;
     public float speed;
     // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            Vector3 position = collision.gameObject.transform.position;
+            Instantiate(explosion, position, collision.gameObject.transform.rotation);
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
