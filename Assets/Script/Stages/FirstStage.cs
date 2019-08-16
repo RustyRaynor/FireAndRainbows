8928016d0e820b050ae42c8308162ba5eef847a4
Alwin_Joshy <flamencoresonance@gmail.com>
2019-08-16 23:35:13 +0400

    more tweeking on AI...

--- Assets/Script/Stages/FirstStage.cs
+++ Assets/Script/Stages/FirstStage.cs
@@ -7,14 +7,45 @@ public class FirstStage : Stage
     Vector3 birthPlace;
     public override void SpawnEnemies()
     {
+        EnemyUnit pickedEnemy = enemyUnitSet[Random.Range(0, enemyUnitSet.Length)];
+        if(currentThreat < maxThreat && pickedEnemy.tire2)
+        {   Debug.Log("Changed...");
+            bool changed = false;
+
+                while(!changed){
+                    pickedEnemy = enemyUnitSet[Random.Range(0, enemyUnitSet.Length)];
+                    if(!pickedEnemy.tire2)changed = true;
+                }
+        }
+        else
+        {
+            while(pickedEnemy.tire2){
+                pickedEnemy = enemyUnitSet[Random.Range(0, enemyUnitSet.Length)];
+            }
+        }
+
         float segX = Screen.width/100;
         float segY = Screen.height/100;
-        int enemyType = Random.Range(0, enemyUnitSet.Length);
         birthPlace = Vector3.zero;
         birthPlace.z = 1;
-        birthPlace.x = segX * enemyUnitSet[enemyType].preferedPoint.x;
-        birthPlace.y = segY * enemyUnitSet[enemyType].preferedPoint.y;
-        Instantiate(enemyUnitSet[enemyType].unit, Camera.main.ScreenToWorldPoint(birthPlace), Quaternion.identity);
+        birthPlace.x = segX * pickedEnemy.preferedPoint.x;
+        birthPlace.y = segY * pickedEnemy.preferedPoint.y;
+        Instantiate(pickedEnemy.unit, Camera.main.ScreenToWorldPoint(birthPlace), Quaternion.identity);
+
+        currentThreat += incrementRate;
+        if(currentThreat >= maxThreat) currentThreat = maxThreat;
+
+    }
+
+    public override void SpawnBoss()
+    {
+        float segX = Screen.width/100;
+        float segY = Screen.height/100;
+        birthPlace = Vector3.zero;
+        birthPlace.z = 1;
+        birthPlace.x = segX * stageBoss.preferedPoint.x;
+        birthPlace.y = segY * stageBoss.preferedPoint.y;
+        Instantiate(stageBoss.unit, Camera.main.ScreenToWorldPoint(birthPlace), Quaternion.identity);
 
     }
 
--- Assets/Script/Stages/Stage.cs
+++ Assets/Script/Stages/Stage.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public abstract class Stage : MonoBehaviour
 {
+    [SerializeField][Tooltip("Max Threat Control")]
+    public int currentThreat, maxThreat, incrementRate = 2;
+
     [System.Serializable]
     public struct EnemyUnit
     {
+        public bool tire2;
         public GameObject unit;
         public Vector2 preferedPoint;
     }
 
     public EnemyUnit[] enemyUnitSet;
+    public EnemyUnit stageBoss;
+    public int maxWaves = 20;
     public float spawnX;
     
     public abstract void SpawnEnemies();
+    public abstract void SpawnBoss();
 }
